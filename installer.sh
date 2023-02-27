61c907784a839013899df2885d5a77c8a6bf67b8
Jacob <54694270+BananasRule@users.noreply.github.com>
2023-02-27 22:24:28 +0800

    Create installer.sh
    A one click installer

---
+++ installer.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#Check that script is running as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+#License agreement
+#Make user aware under which license they are installing the software and get their agreement
+echo "Preparing to install DDNS-Service"
+echo "Please agree to the license terms to continue: "
+echo "This software is licenced under MPL2.0, a copy of which can be found here: https://www.mozilla.org/en-US/MPL/2.0/"
+echo "This license includes a disclaimer of warranty and limitation of liability."
+echo "By agreeing you explicitly agree to the disclaimer of warranty and limitation of liability in addition agreeing to the full license."
+echo "By agreeing you declare you have read, understood and have the capability to enter into and agree this license."
+#Force user to read above paragraph
+sleep 5
+#Check acceptance with agreement
+read -p "Do you agree to the license terms? (Y/N): " acceptance
+acceptance=${acceptance^^}
+if [ ${acceptance} = "Y" ]
+then
+#Install dependencies 
+echo "Thank you for agreeing to the license terms. Installation will now begin."
+apt update
+apt upgrade -y
+apt install unzip -y
+apt install python3 -y
+apt install python3-pip -y
+pip install requests 
+
+#Create folder
+cd /opt
+mkdir DDNS-Servicev2
+cd DDNS-Servicev2
+#Download software from latest release and unzip
+wget https://github.com/BananasRule/DDNS-Servicev2/releases/latest/download/DDNS-Service-v2-Release.zip
+unzip DDNS-Service-v2-Release.zip 
+rm DDNS-Service-v2-Release.zip
+
+#Create directory config and copy example files
+mkdir config
+cp example_dnsconfig.conf config/dnsconfig.conf
+cp example_mailconfig.conf config/mailconfig.conf
+#Open example files for user to enter data to
+cd config
+nano dnsconfig.conf
+nano mailconfig.conf
+
+#Protect folder from unauthorised access
+cd /opt
+chmod 700 -R DDNS-Servicev2
+
+#Add cronjob to run every minute
+crontab -l > tempcron
+echo "* * * * * cd /opt/DDNS-Servicev2 && python3 /opt/DDNS-Servicev2/DDNSUpdateService.py" >> tempcron
+crontab tempcron
+rm tempcron
+service cron restart
+
+echo "Installation complete"
+
+fi
